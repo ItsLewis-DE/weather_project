e71a6c26ceda55198963cbe35023508849a3f194
ItsLewis-DE <thanhphong1110n@gmail.com>
2026-04-12 08:42:44 +0200

    inserted_data

--- api_request/api_request.py
+++ api_request/api_request.py
@@ -21,9 +21,11 @@ def fetch_weather_data():
             response = requests.get(url_base,params = params,timeout=30)
             response.raise_for_status()
             print("API reponse received succesfully.")
-            return request.json()
+            data= response.json()
+            print(json.dumps(data,indent=4))
         except requests.exceptions.RequestException as e:
             print(f"An error occured: {e}")
             raise
 
-fetch_weather_data()
+def mock_fetch_data():
+    return {'request': {'type': 'City', 'query': 'New York, United States of America', 'language': 'en', 'unit': 'm'}, 'location': {'name': 'New York', 'country': 'United States of America', 'region': 'New York', 'lat': '40.714', 'lon': '-74.006', 'timezone_id': 'America/New_York', 'localtime': '2026-04-12 01:23', 'localtime_epoch': 1775956980, 'utc_offset': '-4.0'}, 'current': {'observation_time': '05:23 AM', 'temperature': 11, 'weather_code': 113, 'weather_icons': ['https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0008_clear_sky_night.png'], 'weather_descriptions': ['Clear '], 'astro': {'sunrise': '06:22 AM', 'sunset': '07:33 PM', 'moonrise': '03:57 AM', 'moonset': '02:16 PM', 'moon_phase': 'Waning Crescent', 'moon_illumination': 33}, 'air_quality': {'co': '205.85', 'no2': '19.25', 'o3': '57', 'so2': '4.35', 'pm2_5': '7.25', 'pm10': '7.45', 'us-epa-index': '1', 'gb-defra-index': '1'}, 'wind_speed': 14, 'wind_degree': 356, 'wind_dir': 'N', 'pressure': 1030, 'precip': 0, 'humidity': 35, 'cloudcover': 0, 'feelslike': 9, 'uv_index': 0, 'visibility': 16, 'is_day': 'no'}}
---
+++ api_request/insert_record.py
@@ -0,0 +1,84 @@
+from api_request import mock_fetch_data
+import psycopg2
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+def connect_to_db():
+    print("Connecting to database...")
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="weather_project",
+            port=5433,
+            user= os.getenv("user_database"),
+            password=os.getenv("user_password")
+        )
+        return conn
+    except psycopg2.Error as e:
+        print(f"An error occurred while connecting to the database: {e}")
+        raise
+
+def create_table(conn):
+    print("Creating table if not exists...")
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("""
+                CREATE SCHEMA IF NOT EXISTS dev;
+                CREATE TABLE IF NOT EXISTS dev.raw_weather_data (
+                    id SERIAL PRIMARY KEY,
+                    city TEXT,
+                    temperature FLOAT,
+                    weather_description TEXT,
+                    wind_speed FLOAT,
+                    time TIMESTAMP,
+                    inserted_at TIMESTAMP DEFAULT NOW(),
+                    utc_offet TEXT
+                );
+            """)
+            conn.commit()
+            print("Table created successfully.")
+    except psycopg2.Error as e:
+        print(f"An error occurred while creating the table: {e}")
+        raise
+def insert_weather_data(conn,weather_data):
+    print("Inserting weather data into database...")
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("""
+                INSERT INTO dev.raw_weather_data(
+                    city,
+                    temperature,
+                    weather_description,
+                    wind_speed,
+                    time,
+                    inserted_at,
+                    utc_offet
+                )
+                VALUES(%s,%s,%s,%s,NOW(),%s,%s)  
+            """, (weather_data['location']['name'],
+            weather_data['current']['temperature'],
+            weather_data['current']['weather_descriptions'][0],
+            weather_data['current']['wind_speed'],
+            weather_data['location']['localtime'],
+            weather_data['location']['utc_offset']
+            ))
+        conn.commit()
+        print("Data successfully inserted")
+    except psycopg2.Error as e:
+        print(f"Error with {e}")
+        raise
+def main ():
+    try:
+        data=mock_fetch_data()
+        conn=connect_to_db()
+        create_table(conn)
+        insert_weather_data(conn,data)
+    except Exception as e:
+        print(f"An error:{e}")
+        raise
+    finally:
+        if 'conn' in locals():
+            conn.close()
+            print("Database connection closed.")
+main()
